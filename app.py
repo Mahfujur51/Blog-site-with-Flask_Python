95740b6683ad8114e3fcbadc5e1d8f5add641c4e
Mahfujur51 <overcastmoon@gmail.com>
2020-07-27 07:15:21 +0600

    bug fixed in internal server error

--- app.py
+++ app.py
@@ -1,20 +1,50 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flask_blog'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-@app.route('/')
-def index():
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    '''
+    sno, name phone_num, msg, date, email
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    number = db.Column(db.String(12), nullable=False)
+    message = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
+
+@app.route("/")
+def home():
     return render_template('index.html')
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return  render_template('about.html')
-@app.route('/contact')
+    return render_template('about.html')
+
+
+@app.route("/contact", methods = ['GET', 'POST'])
 def contact():
-    return  render_template('contact.html')
-@app.route('/post')
-def post():
-     return  render_template('post.html')
+    if(request.method=='POST'):
+        '''Add entry to the database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        number = request.form.get('number')
+        message = request.form.get('message')
+        entry = Contacts(name=name, number = number, message = message, date= datetime.now(),email = email )
+        db.session.add(entry)
+        db.session.commit()
+    return render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
